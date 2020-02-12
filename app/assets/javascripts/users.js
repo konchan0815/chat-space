43ff4786055057414dc60734ac74a138552bd879
konchan0815 <konchan_shochan_0815@yahoo.co.jp>
2020-02-12 18:20:09 +0900

    イベント時に非同期通信できるようにする

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,17 @@
 $(function () {
-  
   $('#user-search-field').on('keyup', function(e){
     let input = $('#user-search-field').val();
-    console.log(input);
+    $.ajax({
+      type: 'GET',    //HTTPメソッド
+      url: '/users',       //users_controllerの、indexアクションにリクエストの送信先を設定する
+      dataType: 'json',
+      data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+    })
+      .done(function(users) {
+        console.log("成功です");
+      })
+      .fail(function() {
+        console.log("失敗です");
+      });
   });
-
 });
