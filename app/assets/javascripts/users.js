ad63dc60b8b0db91db487f84bc4a3e296768f846
konchan0815 <konchan_shochan_0815@yahoo.co.jp>
2020-02-13 19:55:20 +0900

    インクリメンタルサーチ　完了

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,46 @@
 $(function () {
+
+  function  addUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>
+              `
+    $('#user-search-result').append(html)
+  }
+
+  function  addNoUser(){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+              </div>`
+    $('#user-search-result').append(html)
+  }
+
   $('#user-search-field').on('keyup', function(e){
     let input = $('#user-search-field').val();
     $.ajax({
-      type: 'GET',    //HTTPメソッド
-      url: '/users',       //users_controllerの、indexアクションにリクエストの送信先を設定する
+      type: 'GET',
+      url: '/users',
       dataType: 'json',
-      data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+      data: { keyword: input },
     })
       .done(function(users) {
-        console.log("成功です");
+        $('#user-search-result').empty();
+          if (users.length !== 0) {
+            users.forEach(function(user) {
+              addUser(user);
+              console.log(user);
+            });
+          } else if (input.length == 0) {
+            return false;
+          } else {
+            addNoUser();
+          }
       })
       .fail(function() {
-        console.log("失敗です");
+        alert("ユーザー検索に失敗しました");
       });
   });
 });
